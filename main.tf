6d86cd99915031741af8c4ece4a7a53633a3878d
Benoit Blais <benoit.blais@hashicorp.com>
2026-02-01 15:17:14 -0500

    v0.0.2

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "random_id" "this" {
 
 module "repositories" {
   source  = "app.terraform.io/benoitblais-hashicorp/repositories/github"
-  version = "0.0.2"
+  version = "0.0.1"
 
   name        = "${var.repository_name}${random_id.this.id}"
   description = var.repository_description
